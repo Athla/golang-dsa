f3a8b2a8f444d6428fc49d6c53e71b4ae2ba5362
guilherme.grimm <guilher.c.rodrigues@gmail.com>
2024-05-20 21:03:51 -0300

    feat. impl 217_ContainsDuplicate

---
+++ LC/217_ContainsDuplicate.go
@@ -0,0 +1,18 @@
+package lc
+
+func containsDuplicate(nums []int) bool {
+	m := make(map[int]struct{})
+
+	if len(nums) <= 1 {
+		return false
+	}
+	for _, v := range nums {
+		_, ok := m[v]
+		if ok {
+			return true
+		}
+		m[v] = struct{}{}
+	}
+
+	return false
+}
---
+++ LC/main.go
@@ -0,0 +1,8 @@
+package lc
+
+import "fmt"
+
+func main() {
+	var nums []int = []int{1, 2, 3, 1}
+	fmt.Println(containsDuplicate(nums))
+}
