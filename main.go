d38ca912e75fc091ba9b72b0df796e1c04e86250
guilherme.grimm <guilher.c.rodrigues@gmail.com>
2024-08-02 21:24:54 -0300

    feat. clean main

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"fmt"
-	// lc "github.com/Athla/golang-dsa/LC"
-	algo "github.com/Athla/golang-dsa/Algorithms"
 )
 
 func main() {
-	arr := []int{5, 6, 7, 2, 1, 6, 7, 8, 0}
-	fmt.Println(arr)
-	algo.QuickSort(arr)
-	fmt.Println(arr)
+	fmt.Println("This is were you can try the LC or Algorithms. Batteries are not included, so bring your test cases and examples.")
 }
